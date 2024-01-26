684085bdf31153dd7c57fff3fcd21297a74c8e86
nbogie <neillbogie@googlemail.com>
2024-01-26 21:22:59 +0000

    got rid of custom matcher

--- src/dice.test.js
+++ src/dice.test.js
@@ -1,9 +1,7 @@
 const { randomDieRoll } = require("./dice");
-require("./customMatchersForJest");
 
 test("randomDieRoll", () => {
-    const possibilities = [1, 3, 4, 5, 6];
     for (let i = 0; i < 1000; i++) {
-        expect(randomDieRoll()).toBeOneOf(possibilities);
+        expect([1, 2, 3, 4, 5, 6]).toContain(randomDieRoll());
     }
 });
--- src/jest.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    namespace jest {
-        interface Matchers<R, T = {}> {
-            toBeOneOf(expected: T[]): R;
-        }
-    }
-}
-
-export {};
