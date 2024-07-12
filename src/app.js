e70d69f4cbb68385d558a6e47f5ff4aa7b417c95
nbogie <neillbogie@googlemail.com>
2024-07-12 20:38:42 +0100

    update app.js - removing page renders

--- src/app.js
+++ src/app.js
@@ -1,72 +1,21 @@
 const { app } = require("./support/setupExpress");
 const { query } = require("./support/db");
-
 const { randomDieRoll } = require("./dice");
 
-//configure the server's route handlers
 app.get("/", (req, res) => {
-    res.render("pages/index");
-});
-
-app.get("/products.json", async (req, res) => {
-    const dbResult = await query(
-        "select * from " +
-            " products " +
-            " inner join suppliers on products.supplier_id = suppliers.supplier_id " +
-            "limit 20",
-    );
-    const rows = dbResult.rows;
-    res.json(rows);
-});
-
-app.get("/products", async (req, res) => {
-    const dbResult = await query(
-        "select * from " +
-            " products " +
-            " inner join suppliers on products.supplier_id = suppliers.supplier_id " +
-            "limit 20",
-    );
-    const rows = dbResult.rows;
-    res.render("pages/products", { products: rows });
-});
-
-app.get("/one", (req, res) => {
-    res.render("pages/one");
-});
-
-app.get("/two", (req, res) => {
-    res.render("pages/two", {
-        clickedCells: req.session.clickedCells || [],
+    res.json({
+        outcome: "success",
+        message: "hello world!",
     });
 });
 
-app.get("/three", (req, res) => {
-    res.render("pages/three");
-});
-
-app.get("/four", (req, res) => {
-    console.log("session content: ", req.session);
-    res.render("pages/four", { session: req.session });
-});
-
-app.post("/addToSession", (req, res) => {
-    if (!("messages" in req.session)) {
-        req.session.messages = [];
+app.get("/db-check", async (req, res) => {
+    try {
+        const dbResult = await query("select * from my_table");
+        res.json(dbResult.rows);
+    } catch (error) {
+        console.error("error handling db-check: ", error);
     }
-    req.session.messages.push(`${req.body.message} at ${new Date()}`);
-    res.redirect("/four");
-});
-
-app.get("/formDemo", (req, res) => {
-    res.render("pages/formDemo");
-});
-
-app.post("/formDemo", (req, res) => {
-    console.log("req.body is: ", req.body);
-    //Don't forget to sanitise and validate user-supplied data before using it
-    const formData = req.body;
-    //just send it back to the user for now
-    res.json(formData);
 });
 
 app.get("/randomRoll", (req, res) => {
@@ -74,16 +23,6 @@ app.get("/randomRoll", (req, res) => {
     res.send("NUMBER IS : " + number);
 });
 
-app.get("/clickCell/:cellId", (req, res) => {
-    const cells = new Set(req.session.clickedCells || []);
-    console.log("req.session.clickedCells", cells);
-    const num = parseInt(req.params.cellId);
-    if (!isNaN(num)) {
-        cells.add(num);
-    }
-    req.session.clickedCells = Array.from(cells);
-    res.redirect("/two");
-});
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
